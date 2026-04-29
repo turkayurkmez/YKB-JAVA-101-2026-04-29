fbc76e9f3ba12bd5413f8c9828e2bc46f35e8620
türkay ürkmez <turkay.urkmez@outlook.com>
2026-04-29 10:57:23 +0300

    initialize repo & java intro

---
+++ JavaIntro/Hello.java
@@ -0,0 +1,16 @@
+public class Hello
+{
+   public static void main(String[] args) {         
+            System.out.println("Merhaba Java");
+            System.out.println("Bu kodu JVM çalıştıracak!");
+
+    /*
+     *  Java SE
+     *  Java EE, Web servisleri backend araçları
+     *  Java ME, Gömülü kartlar, IoT
+     *  Android 
+     */
+
+   }
+
+}
